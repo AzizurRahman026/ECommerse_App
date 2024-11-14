fdc4595efaa3e683cd89031f2c16298783b17f49
Azizur Rahman <azizur.rahman@orbitax.com>
2024-11-14 12:38:34 +0600

    let's begin! Inshallah I will be done this and explore in details...

--- obj/Debug/net8.0/ECommerse_App.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECommerse_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerse_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba8ddc8d00fd01a6ad02d5292a9866670b30863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerse_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerse_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
